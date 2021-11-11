3e6384977ac20e5c9791eadc5a4a966d47d3b927
Ignacio Garcia <inazio@gmail.com>
2021-11-11 23:01:47 +0100

    cambios en el SRE

--- SRE.java
+++ SRE.java
@@ -3,34 +3,40 @@ import java.util.Scanner;
 public class SRE {
     public static void main(String[] args) {
 
-        Scanner lee = new Scanner(System.in);
+        Scanner teclado = new Scanner(System.in);
         System.out.print("Introduce una secuencia de números: ");
-        String entrada = lee.nextLine();
+        String entrada = teclado.nextLine();
+        String resultado = "";
+        String[] numeros = entrada.split("\\s");
 
-        String resultado = "ES una serie rítmica de enteros";
-        if (!entrada.endsWith(" 0")) {
+        if (numeros.length <= 3) {
             resultado = "NO ES una serie rítmica de enteros";
         } else {
-            String[] numeros = entrada.split("\\s");
+            int numero1 = Integer.parseInt(numeros[0]);
+            int numero2 = Integer.parseInt(numeros[1]);
 
-            if (numeros.length <= 3) {
-                resultado = "NO ES una serie rítmica de enteros";
-            } else {
-                int num1 = Integer.parseInt(numeros[0]);
-                int num2 = Integer.parseInt(numeros[1]);
+            int i = 2;
 
-                int i = 2;
-
-                while (i < numeros.length - 1) {
-                    int num = Integer.parseInt(numeros[i]);
-                    if ((num1 >= num && num2 <= num) || (num2 >= num && num1 <= num)) {
-                        num1 = num2;
-                        num2 = num;
+            while (i < numeros.length - 1) {
+                if (resultado == "") {
+                    int numero = Integer.parseInt(numeros[i]);
+                    if ((numero1 >= numero && numero2 <= numero) || (numero2 >= numero && numero1 <= numero)) {
+                        numero1 = numero2;
+                        numero2 = numero;
                         i++;
                     } else {
                         resultado = "NO ES una serie rítmica de enteros";
-                        break;
                     }
+                }
+            }
+
+            if (resultado == "") {
+                int numero = Integer.parseInt(numeros[i]);
+                if (numero != 0) {
+                    resultado = "NO ES una serie rítmica de enteros";
+                } else {
+                    resultado = "ES una serie rítmica de enteros";
+
                 }
             }
 
